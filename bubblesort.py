1c86973bc24b41b4b7f0e7e64930559cb76b24b8
Tammy Butow <tammy@dropbox.com>
2017-07-02 20:48:56 -0700

    Add bubble sort

---
+++ bubblesort.py
@@ -0,0 +1,14 @@
+def BubbleSort( A ):
+    for i in range( len( A )):
+        for k in range( len( A ) - 1, i, -1 ):
+            if ( A[k] < A[k - 1]):
+                swap( A, k, k -1 )
+
+def swap ( A, x, y ):
+    temp = A[x]
+    A[x] = A[y]
+    A[y] = temp
+
+A = [523,246,933,127,277,321,454,565,220]
+BubbleSort(A)
+print(A)
